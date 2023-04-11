0c59c2756a704187d0a8084795f876b3e589e1ae
michaeleymann <m.eymann@hotmail.ch>
2023-04-11 15:42:29 +0200

    added achievements and optional weappon

--- copycat.js
+++ copycat.js
@@ -1,4 +1,8 @@
 /*
+Ideas
+- Double / Triple Kill Function
+- List Achievements in the End
+
 Improvements To Do
 Bind all the enemy stuff to the player, use the collision object do determine whicht objects to destroy
 Make Better Enemie Ausweichfunktion (Line 325)
@@ -11,6 +15,9 @@ var score = 0;
 var time = 0;
 var mobile = false;
 var enemyTrigger;
+var killCounter;
+var numberOfMenMadeCry = 0;
+var numberOfDoubleKills = 0;
 
 
 // SOUND STUFF
@@ -34,6 +41,7 @@ window.fetch(URL)
 // END SOUND STUFF
 
 //START CRAFTY STUFF
+
 Crafty.init(640,640, document.getElementById('game'));
 
 let billie;
@@ -57,7 +65,8 @@ var assetsObj = {
           map: {
               walker_start: [0, 10],
               walker_middle: [4, 10],
-              walker_end: [8, 10]
+              walker_end: [8, 10],
+              ow: [0,8]
           }
       },
       "assets/enemysprite.png": {
@@ -77,7 +86,9 @@ var assetsObj = {
       tileh: 64,
       map: {
         bubble_1: [0,0],
-        pow: [0,1]
+        slayed: [0,1],
+        spark: [0,3],
+        maleTears: [0,4,4,1]
       }
     }
   }
@@ -155,11 +166,14 @@ Crafty.c("MobileSlay", {
 /**
 * Adds scrolling behaviour to entity
 */
-//OK THIS IS SOME SPAGHETTI CODE SHIT, SCROLLING GENERATES LINES
+//OK THIS IS SOME SPAGHETTI CODE SHIT, SCROLLING 
+// IS A COOMPNENT SHARED BY MANY ENITIES BUT
+// IT ALSO GENERATES BACKGROUND LINES 
+// IF THE ENTITY IS A LEFT HOUSE :)
 // I MIGHT WANNA CHANGE THIS (OR NOT)
 Crafty.c("Scrolling", {
 
-  Scrolling: function(){ //take a as true/false
+  Scrolling: function(){ 
   
   this.bind("UpdateFrame", function(eventData){
     if (this.y < -122 ) {
@@ -180,18 +194,18 @@ Crafty.c("Score", {
     this.y = 10,
     this.z = 2,
     this.bind("EnterFrame", function(){
-      this.text("Score: " + score + "  Time: " + time)
+      this.text("Score: " + score + "  Time: " + time + " Double Kills: " + numberOfDoubleKills + " Men brought to tears: " + numberOfMenMadeCry)
     })
   }
 })
 
 Crafty.c("Bubble", {
   init: function(){
-    startTime = 1000 + Math.random()*3000
+    startTime = 2000 + Math.random()*3000
     spriteNumber = Math.floor(Math.random()*7) //xPos Bubble Spritesheet
     this.attach(Crafty.e("2D, Canvas, bubble_1, SpriteAnimation, Scrolling, Enemy, Collision, selfDestroy")
       .attr({x: this.x, y: this.y-64, z:1, w:64, h:64})
-      .reel("none", 1000, 6,0,1) //empty sprite
+      .reel("none", 1000, 8,0,1) //empty sprite
       .reel("catcall", 1000, spriteNumber, 0, 1)
       .reel("0", 1000, 0, 2, 1)
       .reel("1", 1000, 1, 2, 1)
@@ -199,9 +213,16 @@ Crafty.c("Bubble", {
       .reel("3", 1000, 3, 2, 1)
       .reel("4", 1000, 4, 2, 1)
       .reel("5", 1000, 5, 2, 1)
+      .animate("none")
       .bind("EnterFrame", function(){
         setTimeout(()=>this.animate("catcall"),startTime)
-        if ( this._parent.pushed > 15 ) {this.animate("5");console.log("ACHIEVEMENT: MALE TEARS")}
+        if ( this._parent.pushed > 15 && !this._parent.triggered) {
+          this.animate("5");
+          showAchievement("tears");
+          score+=1;
+          numberOfMenMadeCry += 1;
+          this._parent.triggered = true;}
+        else if ( this._parent.pushed > 15 ) this.animate("5")
         else if ( this._parent.pushed > 12 ) this.animate("4")
         else if ( this._parent.pushed > 9 ) this.animate("3")
         else if ( this._parent.pushed > 6 ) this.animate("2")
@@ -211,6 +232,35 @@ Crafty.c("Bubble", {
     )
   }
 })
+
+
+function multiKillCounter(frame){
+  killDelay = 20;
+  if ( killCounter >= frame - killDelay) {
+    showAchievement("doubleKill");
+    score += 1;
+    numberOfDoubleKills +=1;
+  }
+  killCounter = frame;
+
+}
+/**
+ * Creates an achievement message from the sprite map "bubbles"
+ * The message self destroys after 3 seconds
+ *
+ * @param {string} achv can be "tears", "doubleKill" or "smash"
+ */
+function showAchievement(achv) {
+  achievement = Crafty.e("2D, Canvas, SpriteAnimation, maleTears, Scrolling")
+    .attr({x: 192, y: 300, z: 2})
+    .reel("tears", 1000,0,4,1)
+    .reel("smash", 1000,4,4,1)
+    .reel("doubleKill", 1000,8,4,1)
+    .animate(achv)
+    .bind("EnterFrame", function(){
+      setTimeout(()=>this.destroy(),3000)
+    })
+}
 // Functions
 /**
  * Returns Billie
@@ -224,7 +274,7 @@ billie = Crafty.e('2D, Canvas, walker_start, SpriteAnimation, Twoway, Collision,
   slayCounter: 0
 })
 .twoway(200)
-.collision(26,16,26,48,48,48,48,16) 
+.collision(16,16,16,48,48,48,48,16) 
 .reel("walk", 1000, 0, 10, 8)
 .reel("walkLeft", 1000, 0, 9, 8)
 .reel("walkRight", 1000, 0, 11, 8)
@@ -259,7 +309,6 @@ billie = Crafty.e('2D, Canvas, walker_start, SpriteAnimation, Twoway, Collision,
   if (this.slayCounter > 0 ) {
     this.animationSpeed = 4
     this.state.slay = true;
-
     //play proper animation depending on if walking left, right or straight
     if (this.state.left) {
       if (!this.isPlaying("slayLeft")) this.animate("slayLeft",-1)
@@ -279,6 +328,7 @@ billie = Crafty.e('2D, Canvas, walker_start, SpriteAnimation, Twoway, Collision,
       this.state.slay = false;
     }
   } else {
+
     if (this.state.left && !this.isPlaying("walkLeft")) this.animate("walkLeft",-1)
     else if (this.state.left  && this.isPlaying("walkLeft")) this.resumeAnimation("walkLeft", -1)
     else if (this.state.right && !this.isPlaying("walkRight")) this.animate("walkRight", -1)
@@ -289,7 +339,7 @@ billie = Crafty.e('2D, Canvas, walker_start, SpriteAnimation, Twoway, Collision,
   
 
 })
-.bind('Move', function(evt) { // after player moved
+.bind('Move', function(evt) { // after player moved (WALL COLLISIONS)
   var hitDatas, hitData;
   if ((hitDatas = this.hit('Solid'))) { // check for collision with walls
     hitData = hitDatas[0]; // resolving collision for just one collider
@@ -310,15 +360,39 @@ billie = Crafty.e('2D, Canvas, walker_start, SpriteAnimation, Twoway, Collision,
  *
  * @param {number} x the x position of the animation
  * @param {number} y the y position of the animation
+ * @param {string} type the type of death animation: "slayed" or "splattered"
+ * 
  */
-function deathAnimation(x,y){
-  Crafty.e("2D, Canvas, SpriteAnimation, pow")
-    .attr({x:x, y: y, z: 1, w: 64, h: 64})
-    .reel("pow", 1000, 0, 1, 5)
-    .animate("pow")
+function deathAnimation(x,y,type){
+  Crafty.e("2D, Canvas, SpriteAnimation, slayed")
+    .attr({x:x, y: y-24, z: 1, w: 64, h: 64})
+    .reel("slayed", 1000, 0, 1, 5)
+    .reel("splattered", 1000, 7, 1, 4)
+    .animate(type)
     .bind("EnterFrame", function(){
       this.animationSpeed = 3
-      if (!this.isPlaying("pow")) this.destroy()
+      if (!(this.isPlaying("slayed") || this.isPlaying("splattered"))) this.destroy()
+    })
+}
+
+function sparkAnimation(x,y, bossPosition){
+  if (bossPosition != 510 ) x = x+64;
+  Crafty.e("2D, Canvas, SpriteAnimation, spark")
+    .attr({x:x-32, y: y, z: 1, w: 64, h: 64})
+    .reel("spark", 1000, 0, 3, 4)
+    .animate("spark")
+    .bind("EnterFrame", function(){
+      this.animationSpeed = 3
+      if (!this.isPlaying("spark")) this.destroy()
+    })
+}
+
+function makeOptionalWeapon(){
+  console.log("madeow")
+  optionalWeapon = Crafty.e("2D, Canvas, Collision, ow, OptionalWeapon, Tween")
+    .attr({x: billie.x + 128, z: 1, y: -200, w: 64, h: 64})
+    .bind("EnterFrame", function(){
+      this.x = billie.x + 128;
     })
 }
 
@@ -332,48 +406,64 @@ function makeEnemy(){
   b = Math.floor(Math.random() * 10 ) + 5 //yPos Enemy Spritesheet
   
   enemy = Crafty.e("2D, Canvas, enemy_start, Bubble, SpriteAnimation, Scrolling, Tween, Enemy, Collision")
-  .attr({x: xPos*64, y: 11*64, z:1, scrollSpeed: 1, pushed: 0})
+  .attr({x: xPos*64, y: 11*64, z:1, scrollSpeed: 1, pushed: 0, triggered: false})
   .reel("walk", 1000, a, b, 1)
   .animate("walk")
   .collision(26,16,26,48,48,48,48,16)
   .onHit("Player", function(evt, first){ //first is true if this is the first collision
     if (billie.state.slay && first) {
       this.timeout(function() {
-        deathAnimation(this._x, this._y);
+        deathAnimation(this._x, this._y, "slayed");
         this.destroy()
       }, 100);
     score+=1
+    multiKillCounter(Crafty.frame())
     } else this.tween({y: this.y+40}, 100);this.pushed +=1;
+  })
+  .onHit("OptionalWeapon", function(evt, first){ //first is true if this is the first collision
+    this.timeout(function() {
+      deathAnimation(this._x, this._y, "splattered");
+      this.destroy()
+      }, 100);
+    score+=1
   })
   .Scrolling()
 }
 
+/*
 function makeBoss(){
-  billie.twoway(1)
-  setTimeout(()=>billie.twoway(200), 9000)
+  bossPosition = 510;
+  if ( billie.x > 400 ) bossPosition = 64; // change billie.x to 400
+  billie.twoway(1);
+  setTimeout(()=>billie.twoway(200), 9000);
 
-  clearInterval(enemyTrigger)
   boss = Crafty.e("2D, Canvas, Color, Collision, Tween, Solid, Boss, Scrolling")
-  .attr(({x: 510, y: 11*64, w:64, h:128, z:1, health: 10, dead: false, scrollSpeed: 1}))
+  .attr(({x: bossPosition, y: 11*64, w:64, h:128, z:1, health: 30, dead: false, scrollSpeed: 1}))
   .Scrolling()
   .color("#FF0")
   .bind("EnterFrame", function(){
     if (this.y == billie.y-100) {
       this.scrollSpeed = 0;
-      this.tween({x: 505, y:billie.y}, 2000) // auf höhe Billie bewegen
-    } else if (this.x < 506) {  // wenn bei 505 heranfahren
+      if (bossPosition == 510) this.tween({x: 505, y:billie.y}, 2000) // auf höhe Billie bewegen
+      else this.tween({x:66, y:billie.y},2000) //boss links
+      } else if (this.x < 506 && bossPosition == 510) {  // wenn bei 505 heranfahren
       this.tween({x: Math.max(billie.x+48, 128)}, 400)
+      
+    } else if (this.x > 65 && bossPosition != 510){ // heranfahren wenn boss links
+      this.tween({x: Math.min(billie.x-48, 440)}, 400)
     }
   })
-  /*.onHit("Player", function(evt, first){
+  .onHit("Player", function(evt, first){
     console.log("boss hit billie")
     if (billie.state.slay && first) {
       if (this.health > 0){
         this.health -= 1
+        sparkAnimation(this._x, this._y, bossPosition)
       } else if (!this.dead){
         this.timeout(function() {
-        deathAnimation(this._x, this._y);
+        deathAnimation(this._x, this._y, "splattered");
         this.destroy()
+        showAchievement("smash")
         }, 100);
         this.dead = true;
         bossLevel = false;
@@ -381,13 +471,15 @@ function makeBoss(){
         enemyTrigger = setInterval(makeEnemy,700 + Math.floor(Math.random() * 500 ) )
       }
     }
-  }) // END ONHIT*/
-  
+  }) // END ONHIT
+  // Health Bar
   Crafty.e("2D, Canvas, Color")
     .attr({x:boss.x, y: boss.y, z: 2, w: boss.health, h: 5})
     .color("#F00")
-    .bind("EnterFrame", function(){this.w = 10*boss.health, this.x = boss.x, this.y=boss.y})
+    .bind("EnterFrame", function(){this.w = 3*boss.health, this.x = boss.x, this.y=boss.y})
 }
+*/
+
 
 /**
  * Returns a "LeftHouse" Entity
@@ -463,7 +555,6 @@ function scaleIfDisplayIsSmall() {
     Crafty.viewport.scale(mySize/640);
   }
 };
- 
 
 // ---------- SCENES ----------
 
@@ -503,10 +594,10 @@ Crafty.scene("title", function(){
 Crafty.scene("main", function(){
   scaleIfDisplayIsSmall()
  
-    
-    //Crafty.audio.play("soundtrack")
+
     generateWorld();
     makeBillie();
+    makeOptionalWeapon()
     Crafty.e("Score")
     if (mobile) {
     Crafty.e("MobileLeft")
@@ -514,13 +605,24 @@ Crafty.scene("main", function(){
     Crafty.e("MobileSlay")
     }
 
-    //setTimeout(makeBoss, 0)
-    // MAKE ENEMIES
+    // GAME TIMING
+    // make enemies periodically
     enemyTrigger = setInterval(makeEnemy,700 + Math.floor(Math.random() * 500 ) )
-    
-    //setTimeout(makeBoss(), 30000)
+
+    setTimeout(()=>optionalWeapon.tween({x: billie.x + 128, y: billie.y}, 2000), 54000)
+    setTimeout(()=>optionalWeapon.tween({x: billie.x + 128, y: -200}, 2000), 69000)
+
+
+    // make a boss
+    //setTimeout(makeBoss, 30000)
+    //setTimeout(makeBoss, 142000)
+    // and stop making normal enemies a bit earlier, try 5000
+    //setTimeout(()=>clearInterval(enemyTrigger), 25000)
+    //setTimeout(()=>clearInterval(enemyTrigger), 137000)
+
     //timer just for development, delete later
     setInterval(function(){time +=1}, 1000)
+    showAchievement("smash")
 
 }); // END MAIN SCENE
 
